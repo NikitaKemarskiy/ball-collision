960f1143863487680c9fe2332942169ce534ec18
NikitaKemarskiy <sashapobeditel1980@gmail.com>
2019-03-13 00:45:44 +0200

    Structure of Form class was changed. screenWidth, screenHeight are static values now, Container pane, MyComponent graphics and JPanel options became Form values.

--- src/Form.java
+++ src/Form.java
@@ -1,52 +1,55 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
-import javax.swing.border.EmptyBorder;
 
 class Form extends JFrame {
     // Private
-    private int width = 0;
-    private int height = 0;
-    private Container pane = null;
+    private static int screenWidth; // Screen width in pixels
+    private static int screenHeight; // Screen height in pixels
+    private Container pane;
+    private MyComponent graphics;
+    private JPanel options;
+
+    // Static initialization block
+    static {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+        screenWidth = screenSize.width;
+        screenHeight = screenSize.height;
+    }
 
     // Public
     public Form() {
         super();
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension screenSize = toolkit.getScreenSize();
-        width = screenSize.width;
-        height = screenSize.height;
         pane = this.getContentPane();
         pane.setLayout(new OverlayLayout(pane));
-        this.setSize(screenSize.width / 3, (int) (screenSize.height / 2.5)); // Size
-        this.setLocation(screenSize.width / 2 - this.getWidth() / 2, screenSize.height / 2 - this.getHeight() / 2); // Location
+        this.setSize(new Dimension(screenWidth / 3, (int) (screenHeight / 2.5))); // Size
+        this.setMinimumSize(new Dimension(screenWidth / 3, (int) (screenHeight / 2.5)));
+        this.setLocation(screenWidth / 2 - this.getWidth() / 2, screenHeight / 2 - this.getHeight() / 2); // Location
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
     }
 
     public Form(String title) {
         super(title);
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension screenSize = toolkit.getScreenSize();
-        width = screenSize.width;
-        height = screenSize.height;
         pane = this.getContentPane();
         pane.setLayout(new OverlayLayout(pane));
-        this.setSize(screenSize.width / 3, (int) (screenSize.height / 2.5)); // Size
-        this.setLocation(screenSize.width / 2 - this.getWidth() / 2, screenSize.height / 2 - this.getHeight() / 2); // Location
+        this.setSize(new Dimension(screenWidth / 3, (int) (screenHeight / 2.5))); // Size
+        this.setMinimumSize(new Dimension(screenWidth / 3, (int) (screenHeight / 2.5)));
+        this.setLocation(screenWidth / 2 - this.getWidth() / 2, screenHeight / 2 - this.getHeight() / 2); // Location
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
     }
 
     public void draw(int ball1Size, int ball2Size) {
-        JComponent graphics = new MyComponent(ball1Size, ball2Size);
-        JPanel options = new JPanel(new FlowLayout());
+        graphics = new MyComponent(ball1Size, ball2Size);
+        options = new JPanel(new FlowLayout());
 
-        options.setBorder(new EmptyBorder(10, 10, 10, 10));
+        options.setLocation(15, 15);
         options.setSize(160, 190);
         options.add(new JButton("button"));
         options.add(new JTextField(15));
-        options.setBackground(new Color(0, 0, 0, 70));
+        options.setBackground(new Color(0, 0, 0, 180));
         options.setAlignmentX(0.3f);
         options.setAlignmentY(0.3f);
         options.setOpaque(true);
@@ -54,15 +57,27 @@ class Form extends JFrame {
         pane.add(options);
         pane.add(graphics);
     }
+
+    public void update() {
+        graphics.repaint();
+    }
 }
 
 class MyComponent extends JComponent {
     // Private
     private static int defaultSize = 30;
+    private static Color backgroundColor = new Color(22, 27, 33);
+    private static Color ballColor = new Color(244, 169, 80);
     private int ball1Size;
     private int ball2Size;
 
-    private void drawBalls(Graphics g) {
+    // Public
+    MyComponent(int ball1Size, int ball2Size) {
+        this.ball1Size = ball1Size;
+        this.ball2Size = ball2Size;
+    }
+
+    public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
         int width = getWidth(); // Application width
         int height = getHeight(); // Application height
@@ -77,18 +92,10 @@ class MyComponent extends JComponent {
         Ellipse2D ball1 = new Ellipse2D.Double(0, height - ball1Diameter, ball1Diameter, ball1Diameter);
         Ellipse2D ball2 = new Ellipse2D.Double(width - ball2Diameter, height - ball2Diameter, ball2Diameter, ball2Diameter);
 
-        g2.setPaint(Color.RED);
+        g2.setColor(backgroundColor);
+        g2.fillRect(0, 0, width, height);
+        g2.setPaint(ballColor);
         g2.fill(ball1);
         g2.fill(ball2);
-    }
-
-    // Public
-    MyComponent(int ball1Size, int ball2Size) {
-        this.ball1Size = ball1Size;
-        this.ball2Size = ball2Size;
-    }
-
-    public void paint(Graphics g) {
-        drawBalls(g);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,73 +4,10 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         Form window = new Form("Ball collision");
-        window.draw(1, 4);
+        window.draw(9, 8);
+
+        window.update();
 
         window.setVisible(true);
     }
 }
-
-class myClass {}
-
-/*
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-
-        JPopupMenu popupMenu = new JPopupMenu();
-        JMenuBar menuBar = new JMenuBar();
-        JSlider slider = new JSlider(JSlider.HORIZONTAL,0, 50, 15);
-        JLabel label = new JLabel(Integer.toString(slider.getValue()));
-
-        popupMenu.add(new JMenuItem("Cut"));
-        popupMenu.add(new JMenuItem("Copy"));
-        popupMenu.add(new JMenuItem("Paste"));
-
-        JMenu menuFile = new JMenu("File");
-        JMenu menuEdit = new JMenu("Edit");
-
-        JMenuItem itemNew = new JMenuItem("New");
-        JMenuItem itemOpen = new JMenuItem("Open");
-        JMenuItem itemExit = new JMenuItem("Exit");
-        JMenuItem itemCut = new JMenuItem("Cut");
-        JMenuItem itemCopy = new JMenuItem("Copy");
-        JMenuItem itemPaste = new JMenuItem("Paste");
-
-        itemExit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                System.exit(0);
-            }
-        });
-        itemExit.setAccelerator(KeyStroke.getKeyStroke("ctrl E"));
-
-        menuFile.add(itemNew);
-        menuFile.add(itemOpen);
-        menuFile.addSeparator();
-        menuFile.add(itemExit);
-
-        menuEdit.add(itemCut);
-        menuEdit.add(itemCopy);
-        menuEdit.add(itemPaste);
-
-        menuBar.add(menuFile);
-        menuBar.add(menuEdit);
-
-        slider.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent changeEvent) {
-                label.setText(Integer.toString(slider.getValue()));
-            }
-        });
-        slider.setMinorTickSpacing(5);
-        slider.setMajorTickSpacing(25);
-        slider.setPaintTicks(true);
-        slider.setPaintLabels(true);
-
-        panel.setComponentPopupMenu(popupMenu);
-        panel.add(slider, BorderLayout.WEST);
-        panel.add(label, BorderLayout.CENTER);
-        panel.revalidate();
-
-        window.add(panel, BorderLayout.NORTH);
-        window.setJMenuBar(menuBar);
-        window.revalidate();*/
